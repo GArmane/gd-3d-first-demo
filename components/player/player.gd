d8a95da4fc7ec3f944fbaca2ab90e8d805f10261
Giovanni Armane <giovanniarmane@gmail.com>
2025-08-04 21:40:43 -0300

    (feat) Camera rotation

---
+++ components/player/player.gd
@@ -0,0 +1,15 @@
+class_name Player extends CharacterBody3D
+
+
+@export_range(0.0, 1.0, 0.05) var camera_sensibility: float = 0.3
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		rotation_degrees.y -= event.relative.x * camera_sensibility
+		%Camera3D.rotation_degrees.x -= event.relative.y * camera_sensibility
+		%Camera3D.rotation_degrees.x = clamp(
+			%Camera3D.rotation_degrees.x,
+			-60.0,
+			60.0,
+		)

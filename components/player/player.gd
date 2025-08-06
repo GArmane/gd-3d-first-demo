ab9377412796d1109078d1801ab170755a64cc9e
Giovanni Armane <giovanniarmane@gmail.com>
2025-08-06 12:27:37 -0300

    (feat) character jumping

--- components/player/player.gd
+++ components/player/player.gd
@@ -1,16 +1,26 @@
 class_name Player extends CharacterBody3D
 
 
-@export_range(0.0, 1.0, 0.05) var camera_sensibility: float = 0.3
-@export_range(0.0, 10.0, 0.5) var movement_speed: float = 5.0
+const GRAVITY := 9.8
+
+@export_range(1.0, 1.0, 0.05) var camera_sensibility := 0.3
+@export_range(1.0, 10.0, 0.5) var movement_speed := 5.0
+@export_range(1.0, 10.0, 0.5) var movement_jump_height := 4.0
 
 
 func _physics_process(delta: float) -> void:
 	var input_direction_2d := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	var input_direction_3d := Vector3(input_direction_2d.x, 0.0, input_direction_2d.y)
 	var direction := transform.basis * input_direction_3d
+	if Input.is_action_pressed("jump") and is_on_floor():
+		velocity.y = movement_jump_height
+	elif Input.is_action_just_released("jump") and velocity.y > 0.0:
+		velocity.y = movement_jump_height / 2
+
 	velocity.x = direction.x * movement_speed
 	velocity.z = direction.z * movement_speed
+	velocity.y -= GRAVITY * delta
+
 	move_and_slide()
 
 
